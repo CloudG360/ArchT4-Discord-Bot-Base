fa397ba4985c58e0d2d6dd1f0127710a1738e160
Cloud <StormCloudOfficial@outlook.com>
2018-05-04 20:09:50 +0100

    Started Base Code.
    Added a bot configuration for launch to enable a more versitail bot.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,14 @@ import java.io.Reader;
 
 public class Main {
 
-    public static void Main(String args[]){
+    private static Resources resources;
+
+    public static Resources getResources(){
+        return resources;
+    }
+
+    public static void main(String args[]){
+        resources = new Resources();
 
         //Bot Config
         if(new File("botconfig.txt").exists()) {
@@ -21,8 +28,20 @@ public class Main {
 
             try {
                 Reader reader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(reader);
+
+                String line = "";
+
+                while((line = bufferedReader.readLine()) != null) {
+                    String[] configEntry = line.split("=");
+                    getResources().botAdministratorConfig.put(configEntry[0], configEntry[1]);
+                }
+
+                bufferedReader.close();
+
             } catch (Exception err){
-                System.out.println("CONFIG ACCESS ERROR - SHUTTING DOWN");
+                System.out.println("CONFIG ACCESS ERROR - SHUTTING DOWN:\n");
+                err.printStackTrace();
                 System.exit(2);
             }
         } else {
@@ -31,10 +50,10 @@ public class Main {
         }
         //JDA API - Bot building.
         JDABuilder jdab = new JDABuilder(AccountType.BOT);
-        jdab.setToken("-");
+        jdab.setToken(getResources().botAdministratorConfig.get("token"));
         jdab.setAutoReconnect(true);
-        jdab.setStatus(OnlineStatus.ONLINE);
-        jdab.setGame(Game.watching("Based on ArchT4"));
+        jdab.setStatus(getResources().onlineStatus.get(getResources().botAdministratorConfig.get("online-status")));
+        jdab.setGame(Game.playing(getResources().botAdministratorConfig.get("motd")));
         try{
             jdab.buildBlocking();
         } catch (Exception err){
--- src/main/java/Resources.java
+++ src/main/java/Resources.java
@@ -1,9 +1,24 @@
 package main.java;
 
+import net.dv8tion.jda.core.OnlineStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Resources {
 
-    public static String botToken = "";
-    public static String homeServer = "";
-    public static String centralControlChannel = "";
+    public Map<String, String> botAdministratorConfig;
+    public Map<String, OnlineStatus> onlineStatus;
+
+
+    public Resources(){
+        botAdministratorConfig = new HashMap<>();
+
+        onlineStatus = new HashMap<>();
+        onlineStatus.put("ONLINE", OnlineStatus.ONLINE);
+        onlineStatus.put("IDLE", OnlineStatus.IDLE);
+        onlineStatus.put("DND", OnlineStatus.DO_NOT_DISTURB);
+        onlineStatus.put("OFFLINE", OnlineStatus.INVISIBLE);
+    }
 
 }
