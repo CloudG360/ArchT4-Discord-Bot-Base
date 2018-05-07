912e449ce376ee9518cf6e044cdbfdc2dcf4c51f
CloudGamer360 <16578982+CloudGamer360@users.noreply.github.com>
2018-05-07 07:30:46 +0100

    Revert "Started work on Services"
    This reverts commit b44466ce8695a959e3ccc5134421cdcd18239f35.

--- src/main/java/Resources.java
+++ src/main/java/Resources.java
@@ -1,19 +1,14 @@
 package main.java;
 
-import main.java.services.CommandService;
 import net.dv8tion.jda.core.OnlineStatus;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Resources {
 
     public Map<String, String> botAdministratorConfig;
     public Map<String, OnlineStatus> onlineStatus;
-
-    public List<CommandService> commandServices = new ArrayList<>();
 
 
     public Resources(){
