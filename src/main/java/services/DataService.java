a6123d1b16893c7094241e05b5871b7d65a108c4
CloudGamer360 <16578982+CloudGamer360@users.noreply.github.com>
2018-05-11 22:16:15 +0100

    .
    .

---
+++ src/main/java/services/DataService.java
@@ -0,0 +1,20 @@
+package main.java.services;
+
+public class DataService {
+
+    public Object requestServerData(String location,String path){
+        /*
+        * Plan - https://docs.google.com/spreadsheets/d/1Z-0bdDlv1vF0jKCfcSb09BsKElIwmprIZZDePXKYskE/edit?usp=sharing
+        * The location should be the Server ID and the path should be a path to the data, for example:
+        * UniqueUserData.PHistory -
+        *
+        */
+
+
+
+
+        //Replace with result.
+        return new Object();
+    }
+
+}
