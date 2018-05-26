bf40a762f70db65762a85acffdb0c16d640c4dfd
CloudGamer360 <16578982+CloudGamer360@users.noreply.github.com>
2018-05-26 11:36:51 +0100

    Started move to MySQL DBs
    Started adding the basic MySQL functions to DataService

--- src/main/java/services/CommandService.java
+++ src/main/java/services/CommandService.java
@@ -44,6 +44,8 @@ public class CommandService extends Thread {
                 Main.getResources().commandServices.remove(this);
                 this.interrupt();
                 return;
+
+
             }
         }
 
--- src/main/java/services/DataService.java
+++ src/main/java/services/DataService.java
@@ -3,14 +3,20 @@ package main.java.services;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import com.sun.org.apache.xerces.internal.xs.datatypes.ObjectList;
 import main.java.Main;
 import main.java.Resources;
 import org.json.JSONObject;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,6 +24,41 @@ import java.util.Map;
 
 public class DataService {
 
-    
+    public void retriveEntry(String db, String table, String id, String columnRequest){
+        MysqlDataSource datSource = getDataSource();
+        if(datSource == null){
+            return;
+        }
+
+        Connection con = null;
+
+        try {
+            con = datSource.getConnection();
+
+            //Process Data
+
+            con.close();
+        } catch (Exception err){
+
+        }
+
+
+        //There will be a return statement.
+    }
+
+    public MysqlDataSource getDataSource(){
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUser(Main.getResources().botAdministratorConfig.get("sql-username"));
+        dataSource.setPassword(Main.getResources().botAdministratorConfig.get("#sql-password"));
+        try {
+            dataSource.setAutoReconnect(true);
+        } catch (Exception err){
+            return null;
+        }
+
+
+        dataSource.setServerName(Main.getResources().botAdministratorConfig.get("sql-servername"));
+        return dataSource;
+    }
 
 }
